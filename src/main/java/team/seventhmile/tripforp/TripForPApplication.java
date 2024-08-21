ebee97e0f12262a4a2986e8adc2107a6bc75f98b
onlyplsson <onlyplsson@gmail.com>
2024-08-21 17:03:13 +0900

    init

---
+++ src/main/java/team/seventhmile/tripforp/TripForPApplication.java
@@ -0,0 +1,13 @@
+package team.seventhmile.tripforp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripForPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripForPApplication.class, args);
+    }
+
+}
