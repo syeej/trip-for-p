f5f97960ce17ed42b46f01bd506cece36ec4544f
choieuihyun <amammmaa1@naver.com>
2024-08-27 20:04:08 +0900

    [#39] ✨feat: ReviewPostRepositoryCustom 구현

---
+++ src/main/java/team/seventhmile/tripforp/domain/review_post/repository/ReviewPostRepositoryCustom.java
@@ -0,0 +1,11 @@
+package team.seventhmile.tripforp.domain.review_post.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import team.seventhmile.tripforp.domain.review_post.entity.ReviewPost;
+
+public interface ReviewPostRepositoryCustom {
+
+    Page<ReviewPost> getReviewPosts(Pageable pageable);
+
+}
