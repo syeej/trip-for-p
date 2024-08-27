79e82ca3bc65d605e5bfaf13345e21b9d46f99c6
choieuihyun <amammmaa1@naver.com>
2024-08-27 20:04:01 +0900

    [#39] ✨feat: ReviewPostRepository 구현

---
+++ src/main/java/team/seventhmile/tripforp/domain/review_post/repository/ReviewPostRepository.java
@@ -0,0 +1,10 @@
+package team.seventhmile.tripforp.domain.review_post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team.seventhmile.tripforp.domain.review_post.entity.ReviewPost;
+
+@Repository
+public interface ReviewPostRepository extends JpaRepository<ReviewPost, Long>, ReviewPostRepositoryCustom {
+
+}
