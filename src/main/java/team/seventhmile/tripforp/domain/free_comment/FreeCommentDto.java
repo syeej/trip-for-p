f97e04adae75d31d1dbd59e52b6527ff5d389e92
NamK666 <chriscole77@naver.com>
2024-08-28 16:24:25 +0900

    [#66] ✨feat: 자유게시판 댓글 DTO 생성

---
+++ src/main/java/team/seventhmile/tripforp/domain/free_comment/FreeCommentDto.java
@@ -0,0 +1,27 @@
+package team.seventhmile.tripforp.domain.free_comment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FreeCommentDto {
+
+	private Long id;
+
+	@NotBlank(message = "내용을 입력해주세요.")
+	@Size(max = 500, message = "댓글은 500자 이하로 입력해주세요.")
+	private String content;
+
+	private Long postId;
+	private Long authorId;
+
+}
