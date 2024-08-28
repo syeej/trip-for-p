f7d494b1aaa4713ea9bc767e70a62cb3eef2e338
junggyun <onlyplsson@gmail.com>
2024-08-29 00:17:37 +0900

    [#24] :sparkles: feat: 여행 코스 등록 기능 구현

---
+++ src/main/java/team/seventhmile/tripforp/domain/plan/dto/CreatePlanItemRequest.java
@@ -0,0 +1,25 @@
+package team.seventhmile.tripforp.domain.plan.dto;
+
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.seventhmile.tripforp.domain.plan.entity.Place;
+
+@NoArgsConstructor
+@Getter
+public class CreatePlanItemRequest {
+
+    private Place place;
+    private int sequence;
+    private LocalDate tripDate;
+    private String memo;
+
+    @Builder
+    public CreatePlanItemRequest(Place place, int sequence, LocalDate tripDate, String memo) {
+        this.place = place;
+        this.sequence = sequence;
+        this.tripDate = tripDate;
+        this.memo = memo;
+    }
+}
---
+++ src/main/java/team/seventhmile/tripforp/domain/plan/dto/CreatePlanRequest.java
@@ -0,0 +1,30 @@
+package team.seventhmile.tripforp.domain.plan.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.seventhmile.tripforp.domain.plan.entity.Area;
+import team.seventhmile.tripforp.domain.plan.entity.PlanItem;
+
+@NoArgsConstructor
+@Getter
+public class CreatePlanRequest {
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String title;
+    private String area;
+    private List<PlanItemDto> planItems;
+
+    @Builder
+    public CreatePlanRequest(LocalDate startDate, LocalDate endDate, String title, String area,
+        List<PlanItemDto> planItems) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.title = title;
+        this.area = area;
+        this.planItems = planItems;
+    }
+}
