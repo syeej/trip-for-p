bfe562fa6da3c83da8173e26202f0492e302da1d
junggyun <onlyplsson@gmail.com>
2024-08-30 14:24:32 +0900

    [#114] :bug: fix: BaseEntity 수정

--- src/main/java/team/seventhmile/tripforp/global/common/BaseEntity.java
+++ src/main/java/team/seventhmile/tripforp/global/common/BaseEntity.java
@@ -3,11 +3,15 @@ package team.seventhmile.tripforp.global.common;
 import jakarta.persistence.MappedSuperclass;
 import java.time.ZonedDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @MappedSuperclass
 @Getter
+@SuperBuilder
+@NoArgsConstructor
 public abstract class BaseEntity {
     @CreationTimestamp
     private ZonedDateTime createdAt;
