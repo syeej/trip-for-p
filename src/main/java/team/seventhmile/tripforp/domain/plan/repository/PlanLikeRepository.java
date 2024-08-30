0d7b138582c9040b185c3ed0d12c46c822fa2d23
MoonSeokJun <tjrwns2513@naver.com>
2024-08-30 15:14:44 +0900

    [#97] ✨ feat: 여행 코스 좋아요 기준으로 상위 5개 코스 조회

--- src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanLikeRepository.java
+++ src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanLikeRepository.java
@@ -1,6 +1,17 @@
 package team.seventhmile.tripforp.domain.plan.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import team.seventhmile.tripforp.domain.plan.entity.Plan;
 import team.seventhmile.tripforp.domain.plan.entity.PlanLike;
 
-public interface PlanLikeRepository extends JpaRepository<PlanLike, Long> {}
+import java.util.List;
+
+@Repository
+public interface PlanLikeRepository extends JpaRepository<PlanLike, Long> {
+
+    // 좋아요 기준 상위 5개 여행코스 조회
+    @Query("SELECT pl.plan FROM PlanLike pl GROUP BY pl.plan ORDER BY COUNT(pl.id) DESC")
+    List<Plan> findTop5PlansByLikes();
+}
