f0ba6298113f3a65de784842b369d5d0426a88fd
junggyun <onlyplsson@gmail.com>
2024-09-02 13:08:42 +0900

    [#144] :sparkles: feat: 여행 코스 상세 조회 API 연동

--- frontend/src/views/PlanDetailView.vue
+++ frontend/src/views/PlanDetailView.vue
@@ -1,14 +1,29 @@
 <script setup>
 import {useRoute} from "vue-router";
+import {getPlanAPI} from "@/api";
+import {onMounted, ref} from "vue";
 
 const route = useRoute();
 const planId = route.params.planId;
+const plan = ref({});
+
+const getPlan = async function () {
+    try {
+        const response = await getPlanAPI(planId);
+        plan.value = response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+onMounted(() => {
+    getPlan();
+})
 </script>
 
 <template>
     <div>
         여행 코스 상세 조회
-        {{planId}}
+        {{plan}}
     </div>
 </template>
 
