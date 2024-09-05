b13c8dc9d15ebda2c182e399daec95f3abb1f2eb
junggyun <onlyplsson@gmail.com>
2024-09-05 16:50:06 +0900

    [#225] :sparkles: refactor: 장소 등록 request 유효성 추가

--- src/main/java/team/seventhmile/tripforp/domain/plan/dto/CreatePlaceRequest.java
+++ src/main/java/team/seventhmile/tripforp/domain/plan/dto/CreatePlaceRequest.java
@@ -1,7 +1,10 @@
 package team.seventhmile.tripforp.domain.plan.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,12 +28,17 @@ public class CreatePlaceRequest {
 
     @NotBlank(message = "장소 URL은 필수입니다.")
     @Size(max = 255, message = "URL은 255 초과할 수 없습니다.")
+    @Pattern(regexp = "^(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?$", message = "유효한 URL 형식이 아닙니다.")
     private String placeUrl;
 
     @NotNull(message = "경도(X 좌표)는 필수입니다.")
+    @DecimalMin(value = "-90.0", message = "위도는 -90.0 이상이어야 합니다.")
+    @DecimalMax(value = "90.0", message = "위도는 90.0 이하여야 합니다.")
     private double x;
 
     @NotNull(message = "위도(Y 좌표)는 필수입니다.")
+    @DecimalMin(value = "-90.0", message = "위도는 -90.0 이상이어야 합니다.")
+    @DecimalMax(value = "90.0", message = "위도는 90.0 이하여야 합니다.")
     private double y;
 
     @Builder
