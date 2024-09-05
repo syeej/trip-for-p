04aaab23a672de3fe933d35dcc50a7af9e28d697
junggyun <onlyplsson@gmail.com>
2024-09-05 14:46:16 +0900

    [#203] :sparkles: feat: 여행 코스 상세 조회 수정

--- frontend/src/views/PlanDetailView.vue
+++ frontend/src/views/PlanDetailView.vue
@@ -3,6 +3,7 @@
 import {computed, onMounted, ref, watch} from 'vue';
 import {useRoute} from "vue-router";
 import {getPlanAPI} from "@/api";
+import router from "@/router";
 
 const mapContainer = ref(null);
 const currentDateIndex = ref(0);
@@ -238,6 +239,19 @@ const updateMap = async () => {
         map.value.setLevel(3);
     }
 };
+const goBackToSelection = () => {
+    router.push(`/plan/list/${plan.value.area}`);
+};
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${year}.${month}.${day} ${hours}:${minutes}`;
+};
 
 onMounted(() => {
     getPlan();
@@ -252,11 +266,23 @@ watch(currentDate, () => {
 
 <template>
     <div class="plan-detail-view" v-if="plan">
-        <h1>{{ plan.title }}</h1>
+        <img
+            @click="goBackToSelection"
+            class="back-button"
+            src="@/assets/backbutton.png"
+            alt="뒤로가기 버튼"
+        />
+        <div class="title-info">
+            <h1 v-if="plan.startDate !== plan.endDate">{{ plan.title }} / {{ plan.startDate }} ~ {{ plan.endDate }}</h1>
+            <h1 v-else>{{ plan.title }} / {{ plan.startDate }}</h1>
+            <p>{{ formatDate(plan.createdAt) }}</p>
+        </div>
         <div class="plan-info">
-            <p>여행자: {{ plan.writer }}</p>
-            <p>여행 지역: {{ plan.area }}</p>
-            <p>여행 기간: {{ plan.startDate }} ~ {{ plan.endDate }}</p>
+            <span>작성자: {{ plan.writer }}</span>
+            <p>
+                <span>좋아요 {{ plan.likeCount }}</span>
+                <span>조회 {{ plan.views }}</span>
+            </p>
         </div>
 
         <div class="date-navigation" v-if="dates.length > 0">
@@ -284,11 +310,26 @@ watch(currentDate, () => {
 <style scoped>
 .plan-detail-view {
     width: 100%;
-    margin-top: 60px;
+    margin-top: 2em;
 }
-
-.plan-info {
+.title-info {
+    display: flex;
     margin-bottom: 20px;
+}
+.title-info p {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: auto;
+}
+.plan-info {
+    display: flex;
+    margin-bottom: 20px;
+    justify-content: space-between;
+}
+.plan-info p {
+    display: flex;
+    gap: 10px;
 }
 
 .date-navigation {
@@ -350,5 +391,9 @@ watch(currentDate, () => {
 .place-info p {
     margin: 0;
     color: #666;
+}
+.back-button {
+    margin-bottom: 1em;
+    cursor: pointer;
 }
 </style>
