80a62aade38892b2f94f697e0a05ea2791065b0e
HEO <cloudisme99@gmail.com>
2024-09-09 20:26:26 +0900

    [#262] ✨ Feat : 지역 별 추천 여행지 Alan API 생성

--- src/main/java/team/seventhmile/tripforp/external/alan/controller/AlanApiController.java
+++ src/main/java/team/seventhmile/tripforp/external/alan/controller/AlanApiController.java
@@ -1,11 +1,14 @@
 package team.seventhmile.tripforp.external.alan.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team.seventhmile.tripforp.external.alan.dto.AlanApiResponse;
+import team.seventhmile.tripforp.external.alan.dto.AreaRecsRequest;
 import team.seventhmile.tripforp.external.alan.service.AlanApiService;
 
 @RestController
@@ -21,5 +24,13 @@ public class AlanApiController {
         @RequestParam(name = "client_id") String clientId
     ) {
         return alanApiService.processAlanApiRequest(content, clientId);
+    }
+
+    // 여행 지역 기반의 특별한 여행지 또는 체험 추천 서비스
+    @GetMapping("area")
+    public AlanApiResponse processArea(@RequestParam(name = "client_id") String clientId,
+        @Valid @RequestBody AreaRecsRequest request) {
+
+        return alanApiService.getRecommendationsByArea(clientId, request);
     }
 }
--- src/main/java/team/seventhmile/tripforp/external/alan/service/AlanApiService.java
+++ src/main/java/team/seventhmile/tripforp/external/alan/service/AlanApiService.java
@@ -3,11 +3,11 @@ package team.seventhmile.tripforp.external.alan.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import team.seventhmile.tripforp.external.alan.dto.AlanApiResponse;
+import team.seventhmile.tripforp.external.alan.dto.AreaRecsRequest;
 
 @Service
 public class AlanApiService {
@@ -24,6 +24,25 @@ public class AlanApiService {
     public AlanApiResponse processAlanApiRequest(String content, String clientId) {
         content = URLEncoder.encode("2024년 9월 17일부터 2024년 9월 19일까지 " + content + " 여행 코스(숙소, 음식점, 관광지)를 추천해줘", StandardCharsets.UTF_8);
         String url = UriComponentsBuilder.fromHttpUrl("https://kdt-api-function.azurewebsites.net" + "/api/v1/question")
+            .queryParam("content", content)
+            .queryParam("client_id", clientId)
+            .encode()
+            .toUriString();
+        return restTemplate.getForObject(url, AlanApiResponse.class);
+
+    }
+
+    public AlanApiResponse getRecommendationsByArea(String clientId,
+        AreaRecsRequest request) {
+        String area = request.getArea();
+        String startDate = request.getStartDate().toString();
+        String endDate = request.getEndDate().toString();
+        
+        String content = URLEncoder.encode(
+            startDate + "~" + endDate + "까지 " + area + " 지역 기반의 특별한 여행지 또는 체험, 축제 추천해줘",
+            StandardCharsets.UTF_8);
+        String url = UriComponentsBuilder.fromHttpUrl(
+                "https://kdt-api-function.azurewebsites.net" + "/api/v1/question")
             .queryParam("content", content)
             .queryParam("client_id", clientId)
             .encode()
