2c453a57c755bc821ab5a005fda66d4051b00b12
junggyun <onlyplsson@gmail.com>
2024-09-09 23:32:27 +0900

    [#269] :sparkles: feat: 좋아요 유무 체크

--- src/main/java/team/seventhmile/tripforp/domain/plan/controller/PlanLikeController.java
+++ src/main/java/team/seventhmile/tripforp/domain/plan/controller/PlanLikeController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import team.seventhmile.tripforp.domain.plan.dto.CheckPlanLikeRequest;
 import team.seventhmile.tripforp.domain.plan.dto.GetPlanListResponse;
 import team.seventhmile.tripforp.domain.plan.dto.PlanLikeRequestDto;
 import team.seventhmile.tripforp.domain.plan.dto.PlanLikeResponseDto;
@@ -26,11 +27,12 @@ public class PlanLikeController {
   // 좋아요 또는 좋아요 취소를 처리하는 엔드포인트
   @PostMapping
   public ResponseEntity<PlanLikeResponseDto> toggleLikePlan(
-          @AuthenticationPrincipal UserDetails userDetails,
-          @RequestBody PlanLikeRequestDto requestDto) {
+      @AuthenticationPrincipal UserDetails userDetails,
+      @RequestBody PlanLikeRequestDto requestDto) {
 
     // userDetails에서 이메일을 추출하여 서비스에 전달
-    PlanLikeResponseDto responseDto = planLikeService.toggleLikePlan(userDetails.getUsername(), requestDto.getPlanId());
+    PlanLikeResponseDto responseDto = planLikeService.toggleLikePlan(userDetails.getUsername(),
+        requestDto.getPlanId());
     return new ResponseEntity<>(responseDto, HttpStatus.OK);
   }
 
@@ -38,8 +40,17 @@ public class PlanLikeController {
   @PreAuthorize("hasRole('USER')")
   @GetMapping("/me")
   public ResponseEntity<Page<GetPlanListResponse>> getMyFavPlanList(
-          @AuthenticationPrincipal UserDetails user,
-          Pageable pageable){
+      @AuthenticationPrincipal UserDetails user,
+      Pageable pageable) {
     return ResponseEntity.ok(planLikeService.getMyFavPlanList(user, pageable));
+  }
+
+  @PreAuthorize("hasRole('USER')")
+  @GetMapping("/check")
+  public ResponseEntity<Boolean> checkPlanLike(
+      @AuthenticationPrincipal UserDetails user,
+      @RequestBody CheckPlanLikeRequest request
+  ) {
+    return ResponseEntity.ok(planLikeService.checkPlanLike(user.getUsername(), request.getPlanId()));
   }
 }
---
+++ src/main/java/team/seventhmile/tripforp/domain/plan/dto/CheckPlanLikeRequest.java
@@ -0,0 +1,12 @@
+package team.seventhmile.tripforp.domain.plan.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class CheckPlanLikeRequest {
+
+    private Long planId;
+
+}
