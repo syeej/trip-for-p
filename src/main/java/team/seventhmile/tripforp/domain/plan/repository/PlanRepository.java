c3529493ad9e184f1f55c1e83dd7b501204f028b
junggyun <onlyplsson@gmail.com>
2024-09-10 02:36:37 +0900

    [#269] :sparkles: feat: 인기 코스 조회 수정

--- src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanRepository.java
+++ src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanRepository.java
@@ -2,8 +2,11 @@ package team.seventhmile.tripforp.domain.plan.repository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import team.seventhmile.tripforp.domain.plan.dto.GetPopularPlanResponse;
 import team.seventhmile.tripforp.domain.plan.entity.Area;
 import team.seventhmile.tripforp.domain.plan.entity.Plan;
 import team.seventhmile.tripforp.domain.user.entity.User;
@@ -16,4 +19,18 @@ public interface PlanRepository extends JpaRepository<Plan, Long>, PlanRepositor
 
     // 사용자별 Plan 가져오기
     Optional<Plan> findByIdAndUser(Long id, User user);
+
+    @Query("SELECT new team.seventhmile.tripforp.domain.plan.dto.GetPopularPlanResponse(" +
+        "p.id, p.title, SIZE(p.planLikes), " +
+        "(SELECT pl.imageUrl FROM PlanItem pi " +
+        "JOIN pi.place pl " +
+        "WHERE pi.plan.id = p.id AND pl.imageUrl != '' " +
+        "ORDER BY pi.tripDate ASC, pi.sequence ASC " +
+        "LIMIT 1)) " +
+        "FROM Plan p " +
+        "WHERE EXISTS (SELECT 1 FROM PlanItem pi " +
+        "JOIN pi.place pl " +
+        "WHERE pi.plan.id = p.id AND pl.imageUrl != '')" +
+        "ORDER BY SIZE(p.planLikes) DESC ")
+    List<GetPopularPlanResponse> findPopularPlans(Pageable pageable);
 }
