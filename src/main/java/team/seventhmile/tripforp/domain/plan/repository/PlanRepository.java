64d193f95b671f7220cf2dd3431eeedfafb26f80
junhobark <wnsh66@naver.com>
2024-09-10 17:45:45 +0900

    [#261] :sparkles: feat: ai 개인 맞춤형 여행코스 추천 기능 개발

--- src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanRepository.java
+++ src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import team.seventhmile.tripforp.domain.plan.dto.GetPopularPlanResponse;
 import team.seventhmile.tripforp.domain.plan.entity.Area;
 import team.seventhmile.tripforp.domain.plan.entity.Plan;
@@ -34,17 +33,4 @@ public interface PlanRepository extends JpaRepository<Plan, Long>, PlanRepositor
             "WHERE pi.plan.id = p.id AND pl.imageUrl != '')" +
             "ORDER BY SIZE(p.planLikes) DESC ")
     List<GetPopularPlanResponse> findPopularPlans(Pageable pageable);
-
-    // 사용자 ID로 Plan과 관련된 PlanItem, Place 정보를 가져오기
-    @Query("SELECT p.title, p.area, pi.place.placeName " +
-            "FROM Plan p " +
-            "JOIN p.planItems pi " +
-            "WHERE p.user.id = :userId")
-    List<Object[]> findPlansAndPlacesByUserId(@Param("userId") Long userId);
-
-    // 사용자 ID로 PlanLikes 정보를 가져오기
-    @Query("SELECT pl.plan.title, pl.plan.area " +
-            "FROM PlanLike pl " +
-            "WHERE pl.user.id = :userId")
-    List<Object[]> findLikedPlansByUserId(@Param("userId") Long userId);
 }
