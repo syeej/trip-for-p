ad2a0e8bfd801cc80db8b5e3522ffbb85cf3a8ae
junhobark <wnsh66@naver.com>
2024-09-10 17:44:43 +0900

    [#261] :sparkles: feat: ai 개인 맞춤형 여행코스 추천 기능 개발

--- src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanRepository.java
+++ src/main/java/team/seventhmile/tripforp/domain/plan/repository/PlanRepository.java
@@ -2,7 +2,6 @@ package team.seventhmile.tripforp.domain.plan.repository;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
