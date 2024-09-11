b7b8a9282735508f4c44d13871cb1e4708ae96b1
HEO <cloudisme99@gmail.com>
2024-09-11 13:49:04 +0900

    [#290] ✨ Feat : user > dto Validation 추가

--- src/main/java/team/seventhmile/tripforp/domain/user/dto/UserDto.java
+++ src/main/java/team/seventhmile/tripforp/domain/user/dto/UserDto.java
@@ -1,5 +1,7 @@
 package team.seventhmile.tripforp.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,16 @@ import team.seventhmile.tripforp.domain.user.entity.Role;
 @Setter
 public class UserDto {
 
+	@NotBlank(message = "이메일은 필수 입력 값입니다.")
+	@Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
 	private String email;
+
+	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String password;
+
+	@NotBlank(message = "닉네임은 필수 입력 값입니다.")
+	@Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
 	private String nickname;
 	private Role role;
 
