c5c731267fbfc3bd869acc692a5bc175ddfb5f3e
syjoe <syee2joe@gmail.com>
2024-09-16 22:24:23 +0900

    [#308] :necktie: 개인정보수정 로직 변경

--- src/main/java/team/seventhmile/tripforp/domain/user/dto/UserInfoRequest.java
+++ src/main/java/team/seventhmile/tripforp/domain/user/dto/UserInfoRequest.java
@@ -16,7 +16,6 @@ public class UserInfoRequest {
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
-    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
 }
